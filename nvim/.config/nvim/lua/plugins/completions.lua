9c56fa7be98f28bd2c4390edc3d3dabca8d78e7f
sebastian <sebastian.johnsen@knightec.se>
2026-02-20 09:54:25 +0100

    Remap completion to tab

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -38,7 +38,7 @@ return {
 					["<C-k>"] = cmp.mapping.select_prev_item(),
 					["<C-j>"] = cmp.mapping.select_next_item(),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping.confirm({ select = true }),
 				}),
 
 				sources = cmp.config.sources({
