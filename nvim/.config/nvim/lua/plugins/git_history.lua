bfed5f735123f56bb45e2b1e02628ed48c6b16fe
Sebastian <sebastian.johnsen@msn.com>
2026-06-27 15:27:07 +0200

    Fix nvim git history bug

--- nvim/.config/nvim/lua/plugins/git_history.lua
+++ nvim/.config/nvim/lua/plugins/git_history.lua
@@ -1,13 +1,15 @@
 -- plugins/git_history.lua
 return {
-  {
-    "tpope/vim-fugitive",
-    cmd = { "G", "Git", "Gclog", "Gdiffsplit", "Gblame" }, -- lazy load on commands
-  },
-  {
-    "rbong/vim-flog",
-    dependencies = { "tpope/vim-fugitive" },
-    cmd = { "Flog", "Flogsplit" }, -- lazy load on commands
-  },
+	{
+		"tpope/vim-fugitive",
+		cmd = { "G", "Git", "Gclog", "Gdiffsplit" },
+		keys = {
+			{ "<leader>gb", "<cmd>Git blame<CR>", desc = "Git blame current file" },
+		},
+	},
+	{
+		"rbong/vim-flog",
+		dependencies = { "tpope/vim-fugitive" },
+		cmd = { "Flog", "Flogsplit" },
+	},
 }
-
